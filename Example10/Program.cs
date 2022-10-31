d9802bdafb1203196a6b8cb4d4204973009ce80e
Алексей Якунин <116656993+Womxid@users.noreply.github.com>
2022-10-31 12:44:34 +0300

    Add files via upload

---
+++ Example10/Program.cs
@@ -0,0 +1,6 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+System.Console.WriteLine("Введите трехзначное число, чтобы вывести его вторую цифру");
+int num = Int32.Parse(Console.ReadLine());
+int num2 = num%100; 
+int result = num2/10;
+System.Console.WriteLine(result);
