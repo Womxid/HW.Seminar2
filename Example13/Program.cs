30d47a35fb73ea0603cd13426703b6c4c00869c8
Алексей Якунин <116656993+Womxid@users.noreply.github.com>
2022-10-31 12:46:18 +0300

    Add files via upload

---
+++ Example13/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число, чтобы вывести его третью цифру");
+int num = Int32.Parse(Console.ReadLine());
+int num2 = num%1000;
+int result = num2/100;
+if(result==0) System.Console.WriteLine("нет третьей цифры");
+else
+{
+System.Console.WriteLine(result);
+}
---
+++ Example15/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите число которое по порядку обозначат день недели");
+int num = Int32.Parse(Console.ReadLine());
+if(num>7) Console.WriteLine("Такого дня недели нет");
+if(num<1) Console.WriteLine("Такого дня недели нет");
+else if(num == 7) Console.WriteLine("Выходной");
+else if(num == 6) Console.WriteLine("Выходной");
+else if(num == 5) Console.WriteLine("Будний день");
+else if(num == 4) Console.WriteLine("Будний день");
+else if(num == 3) Console.WriteLine("Будний день");
+else if(num == 2) Console.WriteLine("Будний день");
+else if(num == 1) Console.WriteLine("Будний день");
